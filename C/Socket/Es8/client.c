c85e9fd6582c5065354199632771ef68183280ee
James2-2 <150335369+James2-2@users.noreply.github.com>
2025-11-26 22:44:19 +0100

    Update client.c

--- C/Socket/Es8/client.c
+++ C/Socket/Es8/client.c
@@ -2,42 +2,39 @@
 dopo aver effettuato gli opportuni controlli (se necessari), 
 rispedisce al client il numero totale di parole presenti nella stringa.
 */
-
+// CLIENT
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
 
 #define SERVERPORT 1450
 #define DIM 40
 
-int main()
-{
+int main(){
+
     struct sockaddr_in servizio;
-    int socketfd, fromlen = sizeof(servizio);
-    char[DIM] stringa, risposta[DIM];
-    
-    
+    int socketfd;
+    char stringa[DIM], risposta[DIM];
+
     servizio.sin_family = AF_INET;
     servizio.sin_addr.s_addr = htonl(INADDR_ANY);
     servizio.sin_port = htons(SERVERPORT);
-    
-    if((socketfd = socket(AF_INET, SOCK_STREAM, 0)) == -1){
-        printf("Chiamata fallita alla system call socket");
-        exit(1);
-    }
-    
-    if(connect(socketfd, (struct sockaddr*)&servizio, sizeof(servizio)) == -1){
-        printf("Chiamata fallita alla system call connect");
-        exit(1);
-    }
-    
-    printf("Inserisci una stringa");
-    scanf("%s", stringa);
-    
+
+    socketfd = socket(AF_INET, SOCK_STREAM, 0);
+
+    connect(socketfd, (struct sockaddr*)&servizio, sizeof(servizio));
+
+    printf("Inserisci una stringa: ");
+    scanf(" %[^\n]", stringa);
+
     write(socketfd, stringa, sizeof(stringa));
     read(socketfd, risposta, sizeof(risposta));
-    
-    printf("Risposta del server: %s", risposta);
-    
+
+    printf("Risposta del server: %s\n", risposta);
+
     close(socketfd);
-    
     return 0;
 }
