9e2de3ad56587c85d425b03fbdf41fcf14b5486e
James2-2 <150335369+James2-2@users.noreply.github.com>
2025-11-26 22:46:34 +0100

    Update server.c

--- C/Socket/Es8/server.c
+++ C/Socket/Es8/server.c
@@ -2,8 +2,14 @@
 dopo aver effettuato gli opportuni controlli (se necessari), 
 rispedisce al client il numero totale di parole presenti nella stringa.
 */
-
+// SERVER
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#include <unistd.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
 
 #define SERVERPORT 1450
 #define DIM 40
@@ -18,55 +24,36 @@ int contaParole(char stringa[]){
     return count;
 }
 
-int main()
-{
-    struct sockaddr_in servizio;
-    int socketfd, fromlen = sizeof(servizio), soa;
-    char[DIM] stringa, risposta[DIM];
-    
-    
+int main(){
+
+    struct sockaddr_in servizio, remoto;
+    int socketfd, soa;
+    socklen_t fromlen = sizeof(remoto);
+    char stringa[DIM], risposta[DIM];
+
     servizio.sin_family = AF_INET;
     servizio.sin_addr.s_addr = htonl(INADDR_ANY);
     servizio.sin_port = htons(SERVERPORT);
-    
-    if((socketfd = socket(AF_INET, SOCK_STREAM, 0)) == -1){
-        printf("Chiamata fallita alla system call socket");
-        exit(1);
-    }
-    
-    if(bind(socketfd, (struct sockaddr*)&servizio, sizeof(servizio)) == -1){
-        printf("Chiamata fallita alla system call bind");
-        exit(1);
-    }
-    
-    if(listen(socketfd, 10) == -1){
-        printf("Chiamata fallita alla system call listen");
-        exit(1);
-    }
-    
+
+    socketfd = socket(AF_INET, SOCK_STREAM, 0);
+    bind(socketfd, (struct sockaddr*)&servizio, sizeof(servizio));
+    listen(socketfd, 10);
+
     for(;;){
-        printf("Server in ascolto...");
-        
-        if((soa = accept(socketfd, (struct sockaddr*)&servizio, sizeof(servizio))) == -1){
-            printf("Chiamata fallita alla system call accept");
-            exit(1);
-        }
-        
+        printf("Server in ascolto...\n");
+
+        soa = accept(socketfd, (struct sockaddr*)&remoto, &fromlen);
+
         read(soa, stringa, sizeof(stringa));
-        printf("Stringa ricevuta: %s", stringa);
-        
+        printf("Stringa ricevuta: %s\n", stringa);
+
         int nParole = contaParole(stringa);
-        
-        if(nParole > 0){
-            snprintf(risposta, sizeof(risposta), "Sono presenti %d parola/e", nParole);
-        }else{
-            snprintf(risposta, sizeof(risposta), "Non sono presenti parole");
-        }
-        
+
+        snprintf(risposta, sizeof(risposta), "Sono presenti %d parola/e", nParole);
+
         write(soa, risposta, sizeof(risposta));
         close(soa);
     }
-    
-    
+
     return 0;
 }
